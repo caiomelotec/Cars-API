1a2bc0b38e73b94dc263ce033bc58512bfbd0fbe
caiomelotec <caio.melotec@icloud.com>
2024-01-10 23:14:37 +0100

    carSchema

---
+++ models/carModel.js
@@ -0,0 +1,44 @@
+const { Mongoose } = require("mongoose");
+
+monsose = require("mongoose");
+
+const carSchema = Mongoose.Schema(
+  {
+    make: {
+      type: String,
+      required: true,
+    },
+    model: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    engineType: {
+      type: String,
+      required: true,
+      enum: ["Gasoline", "Hybrid", "Electric", "Diesel"],
+    },
+    horsepower: {
+      type: Number,
+      required: true,
+    },
+    torque: {
+      type: Number,
+    },
+    transmissionType: {
+      type: String,
+      required: true,
+      enum: ["Automatic", "Manual"],
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const carModel = mongoose.model("cars", carModel);
