9a48f88ec3dbd1b0a32531a668f69d7a8e8e9a5b
caiomelotec <caio.melotec@icloud.com>
2024-01-13 16:44:20 +0100

    search car by model

--- index.js
+++ index.js
@@ -14,13 +14,36 @@ app.use(express.json());
 app.post("/add-car", (req, res) => {
   const car = req.body;
 
-  carModel.create(car).then((result) => {
-    console.log("Car added successfully");
-    res.send({message: "Car added successfully"})
-  }).catch((err) => {
-    console.log("Error creating car");
-    res.send({message: "Error by adding a car"})
-  })
+  carModel
+    .create(car)
+    .then((result) => {
+      console.log("Car added successfully");
+      res.send({ message: "Car added successfully" });
+    })
+    .catch((err) => {
+      console.log("Error creating car");
+      res.status(500).send({ message: "Error by adding a car" });
+    });
+});
+
+// search car by model
+app.get("/car/:model", (req, res) => {
+  const carName = req.params.model;
+
+  carModel
+    .find({ model: { $regex: carName, $options: "i" } })
+    .then((car) => {
+      if (car.length === 0) {
+        console.log("Car not found");
+        return res.status(404).send({ message: "Car not found" });
+      }
+
+      res.send({ message: "Car was found", data: car });
+    })
+    .catch((err) => {
+      console.log("Error by searching car", err);
+      res.status(500).send({ message: "Error by searching car" });
+    });
 });
 
 app.get("/", (req, res) => {
